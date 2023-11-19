73a061ffd3e33527608192db9cc652c2c4ead7fb
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2023-11-19 09:39:01 +0700

    Update events.py

--- AyiinXd/events.py
+++ AyiinXd/events.py
@@ -200,8 +200,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ UPUTT-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/UputtSupport)"
+                    text = "**✘ MAWING-USERBOT ERROR REPORT ✘**\n\n"
+                    link = "[Team Support](https://t.me/minezulll)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -210,7 +210,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @UputtSupport"
+                    ftext += "\ncukup forward saja pesan ini ke @minezulll"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
