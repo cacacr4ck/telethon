7536a4622895e710f32d8742d0c73870f4ad5ccb
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2023-12-02 14:22:04 +0700

    Update tools.py

--- AyiinXd/ayiin/tools.py
+++ AyiinXd/ayiin/tools.py
@@ -341,8 +341,8 @@ async def bash(cmd):
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Uputt-Userbot"
-    auth_url = "https://github.com/iamuput/Uputt-Userbot"
+    auth_name = "telethon"
+    auth_url = "https://github.com/cacacr4ck/telethon"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
