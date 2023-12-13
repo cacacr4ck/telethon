980ccd91cc7ed9f187e26ddbcfdddbca60065d3d
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2023-12-13 20:10:03 +0700

    Update help.py

--- AyiinXd/modules/help.py
+++ AyiinXd/modules/help.py
@@ -41,7 +41,7 @@ async def helpyins(event):
             chat = "@Botfather"
             try:
                 results = await event.client.inline_query(  # pylint:disable=E0602
-                    BOT_USERNAME, "@uputuserbot"
+                    BOT_USERNAME, "@ownedbyrain_bot"
                 )
                 await results[0].click(
                     event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
